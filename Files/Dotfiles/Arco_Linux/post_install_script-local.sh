e3d0a45431753eed8eb0e4932b4b35282f7df438
Nydoehar <95442777+Nydoehar@users.noreply.github.com>
2025-12-31 15:02:08 -0300

    Update post_install_script-local.sh

--- Files/Dotfiles/Arco_Linux/post_install_script-local.sh
+++ Files/Dotfiles/Arco_Linux/post_install_script-local.sh
@@ -2,8 +2,8 @@
 
 # Script to setup a new Arco Linux installation
 
-sudo mkdir -p /mnt/HDD /mnt/NVME #/mnt/SMB_SHARES/Public /mnt/SMB_SHARES/Private
-sudo mount -t ntfs UUID=2B2F6B4529BC9532 /mnt/HDD
-sudo mount -t ntfs UUID=6B4FA4C31DAB7943 /mnt/NVME
+sudo mkdir -p /mnt/HDD /mnt/NVME 
+sudo mount -t ntfs-3g -o uid=1000,gid=1000,rw,user,exec UUID=2B2F6B4529BC9532 /mnt/HDD
+sudo mount -t ntfs-3g -o uid=1000,gid=1000,rw,user,exec UUID=6B4FA4C31DAB7943 /mnt/NVME
 
-bash /mnt/HDD/MY_STUFF/TECH/ConfigFiles/Files/Dotfiles/Arco_Linux/post_install_script-local.sh
+bash /mnt/HDD/MY_STUFF/TECH/ConfigFiles/Files/Dotfiles/Arco_Linux/post_install_script-local-2.sh
